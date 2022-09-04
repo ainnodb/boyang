50084bd103d91d6d5a43611c11bd366c31dd4090
ainnodb <motingxia@163.com>
2022-09-04 20:21:15 +0800

    modified:   docker/Dockerfile 	modified:   docker/main.go 	chap08/

--- docker/main.go
+++ docker/main.go
@@ -10,15 +10,6 @@ import (
 	"strings"
 )
 
-//自定义abouthandler
-type aboutHandler struct{}
-
-func (m *aboutHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("about!!"))
-}
-
-//自定义abouthandler 结束
-
 func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("200"))
 	log.Print("health check: working")
@@ -50,32 +41,17 @@ func index(w http.ResponseWriter, r *http.Request) {
 		for k, v := range r.Header {
 			fmt.Printf("Header key: %s, Header value: %s \n", k, v[0])
 			w.Header().Set(k, v[0])
-			// w.Header().Set(k, v)
-			// fmt.Fprint(w, k)
-			// fmt.Fprint(w, v[0])
 		}
 	}
 
 	clientip := getClientIP(r)
-	//fmt.Println(r.RemoteAddr)
 	log.Printf("Success! Response code: %d", 200)
 	log.Printf("Success! clientip: %s", clientip)
 }
 
 func main() {
-	about := aboutHandler{}
-	// server := http.Server{
-	// 	Addr: "localhost:8080",
-	// 	// Handler: &mh,  //指定myHandler 就没有了多路路由
-	// 	Handler: nil, //DefaultServe Mux
-	// }
 	http.HandleFunc("/", index)
-	// http.Handle("/", http.FileServer(http.File("index.html")))
-	http.Handle("/about", &about)
 	http.HandleFunc("/healthz", healthzHandler)
-
-	// server.ListenAndServe()
-	// http.ListenAndServe("localhost:8080", http.FileServer(http.Dir("blog"))) //DefaultServe Mux
 	http.HandleFunc("/post", func(w http.ResponseWriter, r *http.Request) {
 		length := r.ContentLength
 		body := make([]byte, length)
@@ -83,23 +59,6 @@ func main() {
 		fmt.Fprintln(w, string(body))
 	})
 
-	// http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
-	// 	url := r.URL
-	// 	query := url.Query()
-
-	// 	id := query["id"]
-	// 	log.Panicln(id)
-
-	// 	name := query.Get("name")
-	// 	log.Panicln(name)
-	// })
-
-	// http.HandleFunc("/process", func(w http.ResponseWriter, r *http.Request) {
-	// 	r.ParseForm()
-	// 	fmt.Fprintln(w, r.Form)
-	// 	fmt.Fprintln(w, r.PostForm)
-	// 	fmt.Fprintln(w, r.FormValue("first_name"))
-	// })
 	mux := http.NewServeMux()
 	// 06. debug
 	mux.HandleFunc("/debug/pprof/", pprof.Index)
